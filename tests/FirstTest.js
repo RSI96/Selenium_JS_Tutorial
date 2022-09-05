d6579402dd7fc419f0873fc6192171ad21378a45
Sikora <radoslaw.sikora@capgemini.com>
2022-09-05 10:37:37 +0200

    Selenium assertions

--- tests/FirstTest.js
+++ tests/FirstTest.js
@@ -1,7 +1,10 @@
-const { By } = require("selenium-webdriver");
+const { By, Key } = require("selenium-webdriver");
 const { Builder } = require("selenium-webdriver");
+const assert = require("assert")
+
 
 async function example() {
+
     const linkToMainPage = "http://petclinic.eastus.cloudapp.azure.com:8888/";
 
     //launch a browser
@@ -12,7 +15,15 @@ async function example() {
     
     //do sth
     await driver.findElement(By.xpath("/html/body/nav/div/div/ul/li[2]/a")).click();
+    await driver.findElement(By.xpath("/html/body/div/div/form/div[1]/div/div/input")).sendKeys("Franklin", Key.RETURN);
     await driver.sleep(2000);
+    
+    //assert
+    let petName = await driver.findElement(By.xpath("/html/body/div/div/table[2]/tbody/tr/td[1]/dl/dd[1]")).getText()
+        .then(function(value) {
+            return value;
+        })
+    assert.strictEqual(petName, "Leo");
 
     //close a browser
     await driver.quit();
